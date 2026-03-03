201a31d34b2cd04b6b76a10ad26774fb3aab36a8
Soham Gugale <sohamgugale170@gmail.com>
2026-03-03 12:08:02 -0500

    fix: use environment variable for database path

--- app/database.py
+++ app/database.py
@@ -1,8 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./finflow.db"
+# Use /tmp on Render (writable), local file in dev
+DB_PATH = os.environ.get("DATABASE_URL", "sqlite:///./finflow.db")
+if DB_PATH.startswith("sqlite"):
+    SQLALCHEMY_DATABASE_URL = DB_PATH
+else:
+    SQLALCHEMY_DATABASE_URL = DB_PATH
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
